52b40a841c7f0361cf9007ba2ee1032ba3db5dec
unknown <sbn9987@naver.com>
2021-04-03 21:05:13 +0900

    test commit

--- config/database.js
+++ config/database.js
@@ -4,12 +4,12 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        host: env('DATABASE_HOST', 'qyaho.ynk2z.mongodb.net'),
+        host: env('DATABASE_HOST'),
         srv: env.bool('DATABASE_SRV', true),
         port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'test-strapi'),
-        username: env('DATABASE_USERNAME', 'sbn9987'),
-        password: env('DATABASE_PASSWORD', '981218as!@'),
+        database: env('DATABASE_NAME'),
+        username: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
       },
       options: {
         authenticationDatabase: env('AUTHENTICATION_DATABASE', 'admin'),
